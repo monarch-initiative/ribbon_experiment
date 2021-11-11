bf6eed497beafe2a843a1f92d8ef4e36ad0c0b77
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-11-11 14:34:49 -0800

    fixing ribbon terms list

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,17 +2,26 @@ import requests
 import simplejson as json
 import pprint
 
-ribbon_terms = ["ZP:0000043",
-                "ZP:0018576",
-                "ZP:0018546",
-                "ZP:0000038",
-                "ZP:0000054",
-                "ZP:0000473",
-                "ZP:0000212",
-                "ZP:0000095",
-                "ZP:0000115"]
+import csv
+
+ribbon_terms = []
+with open('../ribbon.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        if row[2] == 'Sub level':
+            continue
+        else:
+            ribbon_terms.append(row[2])
+    ribbon_terms_imported = dict((rows[0], rows[1]) for rows in reader)
+
+ribbon_categories = list(ribbon_terms_imported)
+print(ribbon_terms)
 
 # add escaping
+
+# TODO: fix querying to find "others" and "all" instead of just returned "true"
+# all across subgroups (ribbon categories = all, other)
+
 escaped_ribbon_terms = [term.replace(":", "\\:") for term in ribbon_terms]
 ribbon_term_facet_queries = {
         term: {
@@ -23,11 +32,7 @@ ribbon_term_facet_queries = {
                 "classes": "unique(phenotypic_feature)"
             }
         } for term in escaped_ribbon_terms}
-host = "localhost"
-port = "8983"
-collection = "phenotype_annotations"
-qt = "query"
-url = 'http://' + host + ':' + port + '/solr/' + collection + '/' + qt + '?'
+url = 'http://localhost:8983/solr/phenotype_annotations/query' + '?'
 print(url)
 
 query = {
@@ -45,11 +50,24 @@ response = requests.post(url,
                          json=query)
 
 result = {}
+
+
+# TODO: reformat this output to the schema requested by the ribbon.
+# http://geneontology.org/docs/ribbon.html
+# https://www.alliancegenome.org/api/gene/*/disease-ribbon-summary?geneID=HGNC:11998
+
 for key, value in response.json()["facets"].items():
     if key != 'count':
         result[key] = {
             "annotations": value["annotations"],
             "classes": value["classes"]
         }
+
+
+
+
+
+# TODO: connect this to the ribbon frontend -- or have the ribbon frontend call this API
+# TODO: where will this API run
 
 pprint.pprint(result)
